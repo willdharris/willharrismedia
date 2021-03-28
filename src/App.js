3df8830282aa0d71397c692a0ea3f42eb6cfd3e3
willdharris <66787651+willdharris@users.noreply.github.com>
2021-03-28 18:25:27 -0400

    added react import back to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
+import React from "react";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <h1>will harris media</h1>
         <h3 className="App-link">coming soon</h3>
       </header>
