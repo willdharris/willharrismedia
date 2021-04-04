2eb9f0294707aef0d28f8b94a158ffed55f432c2
willdharris <66787651+willdharris@users.noreply.github.com>
2021-04-04 19:09:46 -0400

    updated header and styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./App.css";
 
+import Header from "./components/header/header.component";
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="body">
+        <Header />
         <h1>will harris media</h1>
         <h3 className="App-link">coming soon</h3>
       </header>
