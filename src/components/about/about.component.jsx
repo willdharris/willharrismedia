65a7c2542a7b46486ce9993ed953cb02d36c656d
Will <will3harris@gmail.com>
2021-12-27 14:35:14 -0500

    update about.component content

--- src/components/about/about.component.jsx
+++ src/components/about/about.component.jsx
@@ -28,8 +28,8 @@ const About = () => (
                 <li>
                   In my current job, I support cloud applications at IBM and
                   build custom apps to improve business operations. I'm at home
-                  with HTML, CSS, JavaScript, and React, but I love learning new
-                  technologies and languages.{" "}
+                  with HTML, CSS, JavaScript, and React. I love learning and I'm
+                  always studying up on new technologies.{" "}
                 </li>
                 <li>
                   Offline, I'm helping my wife raise our three little humans.
@@ -38,46 +38,6 @@ const About = () => (
                   couch watching football or the Braves.
                 </li>
               </ul>
-              {/* <p className="about-copy">
-                Coming from a background in music and audio engeering, then
-                spending over 7 years in tech, I found my sweet spot in Frontend
-                development. I thrive in the space between the technical and the
-                creative, so I love that I get to be scientifically artistic in
-                building tools that solve problems and help people. I’m a
-                forever learner, always curious to discover the why behind the
-                what. I’m a dot connector and detail stickler. I’m passionate
-                about intentional design and clean code that ultimately helps
-                improve the human experience. <br />
-                In my current job, I support cloud applications at IBM and build
-                custom apps to improve business operations. <br />
-                I’m at home with HTML, CSS, JavaScript, and React, but I love
-                learning new technologies and languages. I'm currently dabbling
-                in Python and Apex for Salesforce. <br />
-                Offline, I’m helping my wife raise our three little humans. And
-                if the stars align, I’m somewhere off the grid on a mountain
-                bike...but realistically I’m probably just on the couch watching
-                football or the Braves.
-              </p> */}
-              {/* <p className="about-copy">
-              I've worked in tech for over 7 years, in various creative,
-              technical, and customer facing roles. But I found my sweet spot in
-              Front End development. I thrive in the space between the creative
-              and the technical, so I love that I get to be artistic{" "}
-              <em>and</em> scientific in building tools that solve problems and
-              help people. I’m a forever learner, always curious to discover the
-              why behind the what. I’m a dot connector and detail stickler. I’m
-              passionate about intentional design and clean code that ultimately
-              helps improve the human experience. <br />
-              In my current job, I support a cloud application at IBM and build
-              custom apps to improve business operations. <br />
-              I’m at home with HTML, CSS, JavaScript, and React, but I love
-              learning new technologies and languages. I'm currently dabbling in
-              Python and Apex for Salesforce. <br />
-              Offline, I’m helping my wife raise our three little humans. And if
-              the stars align, I’m somewhere off the grid on a mountain
-              bike...but realistically I’m probably just on the couch watching
-              football or the Braves.
-            </p> */}
             </div>
           </Col>
           <Col className="image-container">
