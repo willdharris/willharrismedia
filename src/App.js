9c58867336a19ce1ce5745c6cfe8b19185c236e4
Will <will3harris@gmail.com>
2022-02-21 17:09:46 -0500

    add react-ga

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.scss";
-import Header from "./components/header/header.component";
+
+import ReactGA from "react-ga";
+
 import Home from "./components/home/home.component";
 import About from "./components/about/about.component";
 import Work from "./components/work/work.component";
@@ -9,9 +11,12 @@ import Contact from "./components/contact/contact.component";
 import Footer from "./components/footer/footer.component";
 
 function App() {
+  useEffect(() => {
+    ReactGA.initialize("UA-43610616-3");
+    ReactGA.pageview("/");
+  });
   return (
     <div className="App">
-      {/* <Header /> */}
       <Home />
       <About />
       <Work />
